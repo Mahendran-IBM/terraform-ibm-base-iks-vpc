fc20addca2757e078b1f32251b7fa7ddc0d58142
aashiq.jacob@ibm.com <aashiq.jacob@ibm.com>
2026-02-27 09:30:59 +0530

    autoscaling fix

--- examples/advanced/provider.tf
+++ examples/advanced/provider.tf
@@ -11,6 +11,7 @@ provider "kubernetes" {
   host                   = data.ibm_container_cluster_config.cluster_config.host
   token                  = data.ibm_container_cluster_config.cluster_config.token
   cluster_ca_certificate = data.ibm_container_cluster_config.cluster_config.ca_certificate
+  config_path            = data.ibm_container_cluster_config.cluster_config.config_file_path
 }
 
 provider "helm" {
